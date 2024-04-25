84e99e12164cbabfbdf8c92e7b137d3df2c80ed7
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-04-25 20:13:47 -0300

    commit

---
+++ src/components/attendee-list.tsx
@@ -0,0 +1,79 @@
+import { AiOutlineSearch } from "react-icons/ai";
+
+export function AttendeeList() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-3 items-center">
+        <h1 className="text-2xl font-bold">Participantes</h1>
+        <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+          <AiOutlineSearch className="size-4 text-emerald-300 cursor-pointer" />
+          <input
+            type="text"
+            className="bg-transparent flex-1 outline-none"
+            placeholder="Buscar participantes..."
+          />
+        </div>
+      </div>
+
+      <div className="border border-white/10 rounded-lg">
+        <table className="w-full border">
+          <thead>
+            <tr className="border-b border-white/10">
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                <input type="checkbox" />
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Código
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Participantes
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data de inscrição
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left">
+                Data do check-in
+              </th>
+              <th className="py-3 px-4 text-sm font-semibold text-left"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-b border-white/10">
+              <td className="py-3 px-4 text-sm text-zinc-300">
+                <input type="checkbox" />
+              </td>
+              <td className="py-3 px-4 text-sm text-zinc-300">12345</td>
+              <td className="py-3 px-4 text-sm text-zinc-300">
+                <div className="flex flex-col gap-1">
+                  <span className="font-semibold text-white">
+                    Stanley Felix Bergamo
+                  </span>
+                  <span>stanley.felix.bergamo@outlook.com</span>
+                </div>
+              </td>
+              <td className="py-3 px-4 text-sm text-zinc-300">7 dias atrás</td>
+              <td className="py-3 px-4 text-sm text-zinc-300">3 dias atrás</td>
+              <td className="py-3 px-4 text-sm text-zinc-300"></td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td
+                className="py-3 px-4 text-sm text-zinc-300"
+                colSpan={3}
+              >
+                Mostrando 10 de 228 itens
+              </td>
+              <td
+                className="py-3 px-4 text-sm text-zinc-300 text-right"
+                colSpan={3}
+              >
+                página 1 de 23
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
+}
